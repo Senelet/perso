ef6dd76b7e092968a4f6ceb71ccfd8ca6c092aa8
Amadis <amadis.artemis@gmail.com>
2025-05-19 10:02:16 +0200

    first commit

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, AboutMeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
